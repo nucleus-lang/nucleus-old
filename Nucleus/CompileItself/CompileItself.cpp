69b0b9ae1ce0f35734d374eaa2ae28170b718b96
TheNachoBIT <nacholopezcorvalan2@gmail.com>
2022-11-02 10:58:28 -0300

    Adding Linux Support (gotta compile the binary later)

--- Nucleus/CompileItself/CompileItself.cpp
+++ Nucleus/CompileItself/CompileItself.cpp
@@ -4,18 +4,27 @@ void CompileItself::Compile()
 {
 	std::cout << "Compiling Nucleus..." << std::endl;
 
-    std::string getMSBuild = Files::GetPathFromCommand(Files::ConvertToWindowsCmdPath("C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe") + " -latest -prerelease -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -find MSBuild/**/Bin/MSBuild.exe");
-    std::string visualStudioVersion = Files::GetValueFromCommand(Files::ConvertToWindowsCmdPath("C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe") + " -latest -prerelease -products * -property productLineVersion");
-    std::string premakeCmd = "premake5-windows.exe vs" + visualStudioVersion;
+    #ifdef _WIN32
+        std::string getMSBuild = Files::GetPathFromCommand(Files::ConvertToWindowsCmdPath("C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe") + " -latest -prerelease -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -find MSBuild/**/Bin/MSBuild.exe");
+        std::string visualStudioVersion = Files::GetValueFromCommand(Files::ConvertToWindowsCmdPath("C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe") + " -latest -prerelease -products * -property productLineVersion");
+        std::string premakeCmd = "premake5-windows.exe vs" + visualStudioVersion;
+    
+        if (getMSBuild != "")
+        {
+            system(premakeCmd.c_str());
+            system(Files::ConvertToWindowsCmdPath(getMSBuild).c_str());
+            std::cout << "Nucleus Compiled!" << std::endl;
+        }
+        else
+        {
+            std::cout << "No compiler found :c" << std::endl;
+        }
+    #endif
 
-    if (getMSBuild != "")
-    {
-        system(premakeCmd.c_str());
-        system(Files::ConvertToWindowsCmdPath(getMSBuild).c_str());
+    #ifdef __linux__
+        system("chmod -R 777 premake5-linux");
+        system("./premake5-linux gmake2");
+        system("make");
         std::cout << "Nucleus Compiled!" << std::endl;
-    }
-    else
-    {
-        std::cout << "No compiler found :c" << std::endl;
-    }
+    #endif
 }
